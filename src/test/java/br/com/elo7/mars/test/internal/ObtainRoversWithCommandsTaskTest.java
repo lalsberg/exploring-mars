46f7063b2afa6fcc5fd69d32d9baaeeb38d91d8b
lalsberg <leandro.alsberg@gmail.com>
2015-07-06 12:19:08 -0300

    using weld on tests: alternative for Scanner

--- src/test/java/br/com/elo7/mars/test/internal/ObtainRoversWithCommandsTaskTest.java
+++ src/test/java/br/com/elo7/mars/test/internal/ObtainRoversWithCommandsTaskTest.java
@@ -10,7 +10,14 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.enterprise.inject.Alternative;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import org.junit.After;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
@@ -20,16 +27,23 @@ import br.com.elo7.mars.model.enumeration.Command;
 import br.com.elo7.mars.model.enumeration.Direction;
 import br.com.elo7.mars.parser.Parser;
 import br.com.elo7.mars.task.ObtainRoversWithCommandsTask;
+import br.com.elo7.mars.test.setup.WeldJUnit4Runner;
 import br.com.elo7.mars.vo.RoverVO;
 
+@RunWith(WeldJUnit4Runner.class)
 public class ObtainRoversWithCommandsTaskTest {
+	
+	@Inject
+	Scanner scanner;
+	
+	@After
+	public void after() {
+		scanner.close();
+	}
 	
 	@Test
 	public void shouldReadRoversWhileNotReceivedStopCommand() 
 			throws ParseException {
-		
-		ByteArrayInputStream in = twoRoversAndStopCommandInputStream();
-		Scanner scanner = new Scanner(in);
 		
 		Parser parser = mock(Parser.class);
 		when(parser.parseRover(anyString()))
@@ -47,7 +61,18 @@ public class ObtainRoversWithCommandsTaskTest {
 		assertEquals(2, roversWithCommands.size());
 	}
 	
-	private ByteArrayInputStream twoRoversAndStopCommandInputStream() {
+}
+
+@Alternative
+class ScannerProducerTestAlternative {
+	
+	@Produces @Singleton
+    public Scanner get() {
+    	ByteArrayInputStream in = twoRoversAndStopCommandInputStream();
+        return new Scanner(in);
+    }
+    
+    private ByteArrayInputStream twoRoversAndStopCommandInputStream() {
 		String input = "1 2 N\r\n"
 				+ "LMLMLMLMM\r\n"
 				+ "3 3 E\r\n"
@@ -57,5 +82,5 @@ public class ObtainRoversWithCommandsTaskTest {
 				input.getBytes());
 		return in;
 	}
-	
+    
 }
